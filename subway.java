2bab2831bf9115104442a53c571f76f1a3c07c5e
김문희 201611057 <ansgml9884@naver.com>
2016-09-21 01:38:43 +0900

    class Subway

---
+++ subway.java
@@ -0,0 +1,11 @@
+class Subway {
+    int line;
+    void run() {    System.out.println(line+"  lines runs");
+    }
+    void setLine(int l)  {
+        line=l;
+    }
+}
+Subway mySub=new Subway();
+mySub.setLine(3);
+mySub.run();
