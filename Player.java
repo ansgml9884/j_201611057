87f52b8737c40310e7d6913c6d985dc61c06e28f
김문희 /j1_201611057 <ansgml9884@naver.com>
2016-10-23 02:47:36 +0900

    중간 class Player

--- Player.java
+++ Player.java
@@ -1,18 +1,29 @@
-﻿public class Player {
-    private String name=null;
-    int num=0;
-    public Player(String _name) {
-        name=_name;
-    }
-    public void play() {
-        Dice d=new Dice();
-        d.roll();
-        num=d.getNum();
-    }
-    public String getName() {
-        return this.name;
-    }
-    public int getNum() {
-        return this.num;
-    }
+package com.sd.dicegame;
+
+public class Player { 
+    private String name=null; 
+    int num=0; 
+    int score=0;
+    public Player(String _name) { 
+      name=_name; 
+      } 
+    public void play() { 
+      Dice d=new Dice(); 
+      d.roll(); 
+      int num1=d.getNum();
+      d.roll(); 
+      int num2=d.getNum();
+      num=num1+num2;
+      System.out.println(name+" turn on "+num1+" + "+num2+" = "+num);
+      } 
+    public String getName() { 
+      return this.name; 
+      } 
+    public int getNum() { 
+      return this.num; 
+      } 
+    public int getScore(){
+      score=score+1;
+      return score;
+ } 
 }
