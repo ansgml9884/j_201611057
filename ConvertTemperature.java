0674cb906216efb8ef148ac159318bc7aa778983
김문희 /j1_201611057 <ansgml9884@naver.com>
2016-11-02 00:45:56 +0900

    온도 변환 (섭씨<->화씨)

---
+++ ConvertTemperature.java
@@ -0,0 +1,22 @@
+public class ConvertTemperature{
+
+  double temp;
+  String degree;
+
+  public void convertTemperature(double temp,String degree){
+      
+      if(degree.equals("F")){
+          System.out.printf("%.1f"+" C",(temp-32)*5/9); 
+     
+      } else if(degree.equals("C")){ 
+          System.out.printf("%.1f"+" F",temp*9/5+32); 
+     
+      } else{ 
+          System.out.println("Input Error");
+      }
+}
+public static void main(String args[]){
+  new ConvertTemperature().convertTemperature(534,"C"); 
+}
+
+}
