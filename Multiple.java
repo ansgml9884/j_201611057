d6efe23fe4b48af7db49ba2066e15f8b34a198d3
김문희 /j1_201611057 <ansgml9884@naver.com>
2016-11-06 21:19:34 +0900

    3과 5의 배수의 합

---
+++ Multiple.java
@@ -0,0 +1,15 @@
+class Multiple{
+
+  void multiple(){
+    int sum=0;
+    for(int i=0;i<1000;i++){
+      if(i%3==0 || i%5==0){
+        sum+=i;
+      }
+    }
+    System.out.println("Sum = "+sum);
+  }
+  public static void main(String[] args){
+    new Multiple().multiple();
+    }
+ }
