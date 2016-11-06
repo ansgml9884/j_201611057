107f59623f9807992e509b06f293458549841aa3
김문희 /j1_201611057 <ansgml9884@naver.com>
2016-11-06 19:09:38 +0900

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -2,37 +2,36 @@ public class BMI {
 
 	public String bmi(float h, float w) { 
     
-    float bmi = w / (h * h); 
+	float bmi = w / (h * h); 
+	String res;
 		
-		String res;
-		
-		System.out.printf("BMI = %.1f",bmi); 
-		 
-		
-     if (bmi <= 18.5){ 
-        res="low";
-     } else if (bmi >= 18.5 && bmi < 23) { 
-        res="normal"; 
-     } else if (bmi >= 23 && bmi < 25) {
-        res="risky"; 
-		 } else if (bmi >= 25 && bmi < 30) {
-        res="obesity 1"; 
-     } else if (bmi >= 30 && bmi < 40) {
-        res="obesity 2"; 
-     } else if (bmi >= 40) {
-        res="obesity 3"; 
-     } else {
-        res="error"; 
-     } 
-     
-     return res;
+	System.out.printf("BMI = %.1f",bmi); 			
+
+	if (bmi <= 18.5){ 
+        	res="low";
+     	} else if (bmi >= 18.5 && bmi < 23) { 
+        	res="normal"; 
+     	} else if (bmi >= 23 && bmi < 25) {
+        	res="risky"; 
+	} else if (bmi >= 25 && bmi < 30) {
+       		res="obesity 1"; 
+     	} else if (bmi >= 30 && bmi < 40) {
+       		 res="obesity 2"; 
+     	} else if (bmi >= 40) {
+       		res="obesity 3"; 
+    	} else {
+        	res="error"; 
+    	} 
+     	
+	return res;
  }
 
 	public static void main(String[] args) { 
 	  
-	  float weight=(float)58.5;
-    float height=(float)1.7;
+	float weight=(float)58.5;
+    	float height=(float)1.7;
     
-		System.out.print("  Your BMI is "+new BMI().bmi(height,weight)); 
+	System.out.print("  Your BMI is "+new BMI().bmi(height,weight)); 
+	
 	} 
 } 
