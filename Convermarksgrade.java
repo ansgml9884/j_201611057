d7b701f67abf0c9a119297ca1c6a9ba344b505e5
김문희 /j1_201611057 <ansgml9884@naver.com>
2016-11-06 17:18:10 +0900

    성적 학점처리

---
+++ Convermarksgrade.java
@@ -0,0 +1,26 @@
+public class Convermarksgrade{
+  String grade;
+  String convermarksgrade(double marks){  
+    
+    if(90<=marks && marks<=100){  
+       grade="A";
+       
+    } else if (80<=marks && marks<90){  
+        grade="B"; 
+    
+    } else if (70<=marks && marks<80){  
+        grade="C";  
+    } else if (60<=marks && marks<70){  
+        grade="D";  
+    } else{  
+        grade="F or input error";
+    }      
+    return grade;
+  }
+public static void main(String[] args){
+  
+  System.out.printf("Your grade is %s",new Convermarksgrade().convermarksgrade(94)); 
+  
+  }
+
+}
