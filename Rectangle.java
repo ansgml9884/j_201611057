d13eb460e8cf3b2c70eea6f69ac09541a5621943
김문희 /j1_201611057 <ansgml9884@naver.com>
2016-11-27 21:51:58 +0900

    도형선택

--- Rectangle.java
+++ Rectangle.java
@@ -1,35 +1,25 @@
-﻿public class Rectangle{
-private int width;
-private int height;
-public Rectangle(int w, int h){
-this.width=w;
-this.height=h;
+package com.sd.ch8;
+
+public class Rectangle{
+  protected double length;
+  protected double width;
+  protected boolean isSelected;
+  
+  public Rectangle(double l,double w){
+    length=l;
+    width=w;
+  }
+  public double getLength(){
+    return this.length;
+  }
+  public double getWidth(){
+    return width;
+  }
+  public void setSelected(boolean b){
+    this.isSelected=b;
+  }
+  public boolean getSelected(){
+    return isSelected;  
+  }
 }
-public int getWidth(){
-return width;
-}
-public int getHeight(){
-return height;
-}
-public void setHeight(int h){
-height=h;
-}
-public void setWidth(int newWidth){
-width=newWidth;
-}
-public void setSize(int w,int h){
-setWidth(w);
-setHeight(h);
-}
-public int calcArea(){
-return width*height;
-}
-}
-Rectangle rec = new Rectangle(5,7);
-rec.calcArea();
-rec.setSize(7,3)
-rec.setHeight(5);
-rec.calcArea();
-rec.setHeight(4);
-rec.setWidth(5);
-rec.calcArea();
+    
