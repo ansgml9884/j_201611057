95c4619136597c84ecccbeb2d66894046c1b402b
김문희 /j1_201611057 <ansgml9884@naver.com>
2016-12-11 12:25:11 +0900

    IO

---
+++ ReadFile.java
@@ -0,0 +1,22 @@
+package com.sd.io; 
+ 
+import java.io.File; 
+import java.io.FileReader; 
+import java.io.FileNotFoundException; 
+import java.io.IOException; 
+  
+public class ReadFile { 
+  public static void main(String[] args) { 
+		try { 
+			FileReader fr = new FileReader("test.txt"); 
+			int i; 
+			while ((i = fr.read()) != -1) 
+				System.out.println((char)i); 
+			fr.close(); 
+		} catch(FileNotFoundException e) {
+        System.out.println("File not Found");  
+		} catch(IOException e) { 
+		        System.out.println("IOError"); 
+		} 
+	} 
+} 
